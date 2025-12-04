9924672aecc6a20fd7f520275cdad62560550238
RagnorLi <ragnor.li@outlook.com>
2025-12-04 13:02:17 +0800

    Code:init mountain app

--- app/infer-lab/src-tauri/src/lib.rs
+++ app/infer-lab/src-tauri/src/lib.rs
@@ -1,14 +1,14 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_welcome_message() -> String {
+    "会当凌绝顶，一览众山小 ！".to_string()
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_welcome_message])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
